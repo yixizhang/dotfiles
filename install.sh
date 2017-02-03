68e8a51e4db1ccb41ab6cffc60bffe8ade60eb09
yixi zhang <yixi@memsql.com>
2017-02-03 07:53:46 +0000

    fix

--- install.sh
+++ install.sh
@@ -1,22 +1,31 @@
 #! /bin/bash
 
-tmux() {
+install_tmux() {
+    echo "install tmux"
     ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
     tmux source-file $HOME/.tmux.conf
 }
 
-vim() {
-    git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
+install_vim() {
+    echo "install vim"
+    if [ -d ~/.vim_runtime ]
+    then
+        cd ~/.vim_runtime; git pull
+    else
+        git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+        sh ~/.vim_runtime/install_awesome_vimrc.sh
+    fi
     ln -sf $(pwd)/.vimrc $HOME/.vim_runtime/my_configs.vim
 }
 
-shell() {
-    echo "source ~/dotfiles/.bashrc" >> $HOME/.bashrc
+install_shell() {
+    echo "install shell"
+    echo "source $HOME/dotfiles/.bashrc" >> $HOME/.bashrc
+    source $HOME/.bashrc
 }
 
 ## main
-tmux
-vim
-shell
+install_tmux
+install_vim
+install_shell
 
